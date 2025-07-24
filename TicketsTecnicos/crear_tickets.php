2e9980528f88bb62f062e3c0cba2781439bd306a
daniel11778 <danielmosso112@gmail.com>
2025-07-24 11:18:19 -0700

    Tabla con los tickets del usuario

--- TicketsTecnicos/crear_tickets.php
+++ TicketsTecnicos/crear_tickets.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 $host = "localhost";
 $usuario_db = "root";
 $contrasena_db = "";
@@ -10,23 +12,48 @@ $conn = new mysqli($host, $usuario_db, $contrasena_db, $basedatos);
 $descripcion = $_POST['descripcion'];
 $fecha_apertura = $_POST['fecha_apertura'];
 $estado = "pendiente";
-
-$sql = "INSERT INTO tickets (descripcion, estado, fecha_de_apertura) VALUES (?, ?, ?)";
+$id_usuario = $_SESSION['usuario_id'];
+//para crear el ticket
+$sql = "INSERT INTO tickets (descripcion, estado, fecha_de_apertura, id_usuario) VALUES (?, ?, ?, ?)";
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("sss", $descripcion, $estado, $fecha_apertura);
-
-$sql2 = "SELECT id, descripcion, estado, fecha_de_apertura 
-FROM tickets
-WHERE id_usuario = ?";
-$stmt = $conn->prepare($sql2);
-
-
-if ($stmt->execute()) {
-    echo "Ticket creado correctamente.";
-} else {
-    echo "Error al crear el ticket: " . $stmt->error;
-}
+$stmt->bind_param("sssi", $descripcion, $estado, $fecha_apertura,$id_usuario);
 
 $stmt->close();
+
+//para jalar los datos de la base de datos y crear la tabla
+$sql2 = "SELECT id, descripcion, estado, fecha_de_apertura, fecha_de_cierre 
+FROM tickets
+WHERE id_usuario = ?";
+$stmt2 = $conn->prepare($sql2);
+$stmt2->bind_param("i", $id_usuario);
+$stmt2->execute();
+$resultado = $stmt2->get_result();
+
+echo "<table border='1' cellpadding='5'>";
+echo "<tr>
+        <th>ID</th>
+        <th>Descripción</th>
+        <th>Estado</th>
+        <th>Fecha de apertura</th>
+        <th>Fecha de cierre</th>
+      </tr>";
+
+// filas de resultados
+while ($fila = $resultado->fetch_assoc()) {
+    echo "<tr>";
+    echo "<td>" . $fila['id'] . "</td>";
+    echo "<td>" . $fila['descripcion'] . "</td>";
+    echo "<td>" . $fila['estado'] . "</td>";
+    echo "<td>" . $fila['fecha_de_apertura'] . "</td>";
+    echo "<td>" . $fila['fecha_de_cierre'] . "</td>";
+    echo "</tr>";
+}
+
+echo "</table>";
+
+
+
+
+$stmt2->close();
 $conn->close();
 ?>
